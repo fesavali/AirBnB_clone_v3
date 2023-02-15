16653381f46e0843788612d9a10aafa06a343e01
fesavali <felixnzioki99@gmail.com>
2023-02-15 12:23:47 +0300

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,32 +1,37 @@
-#!/usr/bin/env python3
-"""Defines a status route for the HolbertonBnB API."""
-from flask import jsonify
-from flasgger import swag_from
-from models import storage
+#!/usr/bin/python3
+"""
+Flask route that returns json status response
+"""
 from api.v1.views import app_views
+from flask import jsonify, request
+from models import storage
 
 
-@app_views.route("/status")
-@swag_from("../apidocs/status/status.yml")
+@app_views.route('/status', methods=['GET'])
 def status():
-    """Returns the server status.
-    Returns:
-        JSON object with the current server status.
     """
-    return jsonify({"status": "OK"})
+    function for status route that returns the status
+    """
+    if request.method == 'GET':
+        resp = {"status": "OK"}
+        return jsonify(resp)
 
 
-@app_views.route("/stats")
-@swag_from("../apidocs/stats/stats.yml")
+@app_views.route('/stats', methods=['GET'])
 def stats():
-    """Retrives the count of each object type.
-    Returns:
-        JSON object with the number of objects by type."""
-    return jsonify({
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    })
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
